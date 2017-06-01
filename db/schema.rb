446d1c7600508d80a356ce661a5b091bd7603ddf
jc.swift <jc.swift@icloud.com>
2017-06-01 00:15:45 -0500

    Intergrated new and create actions for portoflios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531012032) do
+ActiveRecord::Schema.define(version: 20170601045100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170531012032) do
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
+    t.text     "body"
   end
 
   create_table "portfolios", force: :cascade do |t|
